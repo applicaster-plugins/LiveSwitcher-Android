16e701895411fdf9b10f0f8a38108ed7f23d8ffe
Juan Hernandez <j.hernandez@applicaster.com>
2019-07-17 22:17:53 -0400

    added keys for reminder texts

--- src/main/java/com/applicaster/liveswitcher/screen/adapter/ProgramAdapter.kt
+++ src/main/java/com/applicaster/liveswitcher/screen/adapter/ProgramAdapter.kt
@@ -1,17 +1,15 @@
 package com.applicaster.liveswitcher.screen.adapter
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import com.applicaster.app.CustomApplication
 import com.applicaster.atom.model.APAtomEntry
 import com.applicaster.liveswitcher.R
 import com.applicaster.liveswitcher.model.ChannelModel
 import com.applicaster.liveswitcher.utils.Constants
-import com.applicaster.liveswitcher.utils.Constants.CONF_REMINDER_ASSET
 import com.applicaster.liveswitcher.utils.Constants.EXTENSION_APPLICASTER_CHANNEL_ID
 import com.applicaster.liveswitcher.utils.Constants.EXTENSION_END_TIME
 import com.applicaster.liveswitcher.utils.Constants.EXTENSION_START_TIME
@@ -19,10 +17,11 @@ import com.applicaster.liveswitcher.utils.Constants.PREFERENCE_ITEM_SELECTED_POS
 import com.applicaster.liveswitcher.utils.LiveSwitcherUtil
 import com.applicaster.util.AlarmManagerUtil
 import com.applicaster.util.PreferenceUtil
+import com.applicaster.util.StringUtil
+import com.applicaster.util.ui.Toaster
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.ViewTarget
-import kotlinx.android.synthetic.main.item_program.view.*
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.item_program.view.*
 
 
 class ProgramAdapter(private val items: List<APAtomEntry>, private val channels: List<ChannelModel.Channel>?,
@@ -62,7 +61,6 @@ class ProgramAdapter(private val items: List<APAtomEntry>, private val channels:
             val requestOptions = RequestOptions()
             // set alarm icon depending on if the reminder is set or not
             Glide.with(context)
-                    .setDefaultRequestOptions(requestOptions.placeholder(R.drawable.program_card_placeholder))
                     .load(drawable)
                     .into(holder.ivAlert)
 
@@ -111,18 +109,19 @@ class ProgramAdapter(private val items: List<APAtomEntry>, private val channels:
         }
     }
 
-    private fun toggleReminder(context: Context, position: Int, holder: ProgramViewHolder):
-            ViewTarget<ImageView, Drawable> {
-        return if (AlarmManagerUtil.isAlarmSet(context, items[position].id)) {
+    private fun toggleReminder(context: Context, position: Int, holder: ProgramViewHolder) {
+        if (AlarmManagerUtil.isAlarmSet(context, items[position].id)) {
             listener.removeReminder(items[position])
             Glide.with(context)
                     .load(R.drawable.remove_reminder_asset)
                     .into(holder.ivAlert)
+            Toaster.makeToast(context, holder.removeReminderText)
         } else {
             listener.addReminder(items[position])
             Glide.with(context)
                     .load(R.drawable.set_reminder_asset)
                     .into(holder.ivAlert)
+            Toaster.makeToast(context, holder.setReminderText)
         }
     }
 
@@ -141,6 +140,8 @@ class ProgramViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     var tvIsWatching = view.tv_is_watching
     var ivChannel = view.iv_channel
     var tvLiveEvent = view.tv_live_event
+    var setReminderText: String? = null
+    var removeReminderText: String? = null
 
     init {
         tvProgramName.setTextColor(LiveSwitcherUtil.parseColor(
@@ -168,5 +169,14 @@ class ProgramViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                 LiveSwitcherUtil.getParam(Constants.CONF_WATCHING_TAG_TEXT_FONTSIZE))
         tvLiveEvent.setBackgroundColor(LiveSwitcherUtil.parseColor(
                 LiveSwitcherUtil.getParam(Constants.CONF_LIVE_EVENT_TAG_TEXT_BACKGROUND_COLOR)))
+
+        LiveSwitcherUtil.getParam(Constants.CONF_SET_REMINDER_TEXT).let {
+            setReminderText = if(it != "null") { it } else { CustomApplication.getAppContext().getString(R.string.reminder_set) }
+        }
+
+        LiveSwitcherUtil.getParam(Constants.CONF_REMOVE_REMINDER_TEXT).let {
+            removeReminderText = if(it != "null") { it } else { CustomApplication.getAppContext().getString(R.string.reminder_removed) }
+        }
+
     }
 }
